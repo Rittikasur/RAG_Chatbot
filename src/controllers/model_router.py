ad6be9b066ab115beef45e53a8d892fc7b0f8d34
Rohit Sinha <rohit123sinha456@gmail.com>
2025-02-05 12:45:22 +0530

    LLM Router for general convo

---
+++ src/controllers/model_router.py
@@ -0,0 +1,60 @@
+from langchain_core.runnables import RunnableLambda
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+
+ollamamodel = OllamaLLM(model="llama3.2:1b")
+chain = (
+        PromptTemplate.from_template(
+            """
+            You are an expert that can understand if the user is trying to have a general conversation
+            or is asking about doubts and questions and specific subject topics
+            Given the user question below, classify it as either being about `Subject Topic` or `Others`.
+
+    Do not respond with more than one word.
+
+    <question>
+    {question}
+    </question>
+
+    Classification:"""
+        )
+        | ollamamodel
+        | StrOutputParser()
+)
+
+general_chain = (PromptTemplate.from_template(
+    """You are an expert in general conversation.
+Respond to the following question:
+
+Question: {question}
+Answer:"""
+) | ollamamodel | StrOutputParser() )
+
+answerchain = (
+        PromptTemplate.from_template(
+            """
+            You are an expert that can understand the context. Give a explanation or answer as a teacher to the
+            student's question from the context.
+
+    <context>
+    {context}
+    </context>
+
+    <question>
+    {question}
+    </question>
+    """
+        )
+        | ollamamodel
+        | StrOutputParser()
+)
+
+
+def llmroute(question,context):
+    classification_result = chain.invoke({"question": question})
+    if "Subject Topic" not in classification_result.lower():
+        response = general_chain.invoke({"question": question})
+    else:
+        response = answerchain.invoke({"context": context, "question": question})
+    return response
--- src/main.py
+++ src/main.py
@@ -5,6 +5,18 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.docstore.document import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from flask_cors import CORS
+import jwt
+import flask
+import requests
+from controllers.auth import authenticateToken
+from controllers.model_router import llmroute
+from controllers.router import getContext
+import psycopg2
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+
+ollamamodel = OllamaLLM(model="llama3.2:1b")
 
 model = HuggingFaceEmbeddings(model_name="sentence-transformers/multi-qa-mpnet-base-cos-v1")
 
@@ -12,7 +24,6 @@ pc_apikey = "pcsk_6gdQRf_7G1DD6mozXD3NmdFYEc6TU9fcQMdM9USPxrjdt8qBBsrmiPt9EKtxyF
 pinecone3 = PineconeVectorStore(pinecone_api_key=pc_apikey, embedding=model, index_name="topic-store2")
 pinecone4 = PineconeVectorStore(pinecone_api_key=pc_apikey, embedding=model, index_name="content-store")
 
-import psycopg2
 conn = psycopg2.connect(
     dbname="postgres",
     user="root",
@@ -25,17 +36,14 @@ conn = psycopg2.connect(
 #cursor.execute("SET search_path = lmstest")
 
 
-import jwt
+
 SECRET_KEY = "whyisitasitis"
 
-import flask
-import requests
+
 app = flask.Flask(__name__)
 CORS(app)
 
 
-from controllers.auth import authenticateToken
-from controllers.router import getContext
 
 @app.route("/", methods=["GET"])
 def home():
@@ -79,39 +87,6 @@ def updateChat():
 def deleteChat():
     return "Not Implemented", 501
 
-
-# @app.route("/router/topic", methods=["POST"])
-# def routeTopic():
-#     data = flask.request.json
-#     query = data.get("query")
-#     course_id = data.get("course_id")
-#     if query is None or course_id is None:
-#         return "Bad Request", 400
-
-#     filter = { "course_id": { "$eq": course_id } }
-#     results = pinecone3.similarity_search_with_score(query, k=3, filter=filter)
-
-#     return flask.jsonify([
-#         { "metadata": doc.metadata, "topic": doc.page_content, "score": score } 
-#         for doc, score in results
-#     ]), 200
-
-# @app.route("/router/content", methods=["POST"])
-# def routeContent():
-#     data = flask.request.json
-#     query = data.get("query")
-#     topic_id = data.get("topic_id")
-#     if query is None or topic_id is None:
-#         return "Bad Request", 400
-
-#     # filter = { "topic_id": { "$eq": topic_id } }
-#     filter = {}
-#     results = pinecone4.similarity_search_with_score(query, k=5, filter=filter)
-
-#     return flask.jsonify([
-#         { "metadata": doc.metadata, "content": doc.page_content, "score": score }
-#         for doc, score in results
-#     ]), 200
 
 @app.route("/content", methods=["POST"])
 def createContent():
@@ -166,10 +141,52 @@ def createContent():
     return flask.jsonify({"content_id": content_id}), 200
 
 
+
+@app.route("/route", methods=["POST"])
+def route():
+    cursor = conn.cursor()
+    user_id = 8  # authenticateToken()
+    if not user_id:
+        return "Unauthorized", 401
+    try:
+        cursor.execute("SELECT course_id FROM users WHERE user_id = %s", (user_id,))
+        data = flask.request.json
+        query = data.get("query")
+        course_id = cursor.fetchone()[0]
+        if not course_id:
+            return "Unauthorized", 401
+
+        context, topic_ids = getContext(query, course_id)
+
+        chain = (
+                PromptTemplate.from_template(
+                    """
+                    You are an expert that can understand the context. Give a explanation or answer as a teacher to the
+                    student's question from the context.
+        
+            <context>
+            {context}
+            </context>
+            
+            <question>
+            {question}
+            </question>
+            """
+                )
+                | ollamamodel
+                | StrOutputParser()
+        )
+        ctx = ''.join(context)
+        response = chain.invoke({"context":ctx,"question": query})
+        return flask.jsonify({"answer": response}), 200
+    except Exception as e:
+        return flask.jsonify({"error":str(e)}), 500
+
+
 @app.route("/query", methods=["POST"])
 def query():
     cursor = conn.cursor()
-    user_id = authenticateToken()
+    user_id = 8 #authenticateToken()
     if not user_id:
         return "Unauthorized", 401
     try:
@@ -180,37 +197,12 @@ def query():
 
         data = flask.request.json
         query = data.get("query")
-        # context = data.get("context")
-        stream = data.get("stream")
-        if not stream: stream = False
-        
         context, topic_ids = getContext(query, course_id)
-    
-        rag_query = f"""
-            context: {context}
-            prompt: {query}
-        """
-    
-        response = requests.post("http://localhost:11434/api/generate", json={
-            "model": "llama3.2:1b",
-            "prompt": rag_query,
-            "stream": stream
-        }, stream=stream)
+        ctx = ''.join(context)
+        response = llmroute(query,ctx)
 
-        if stream:
-            response = ""
-            def generate_response():
-                yield json.dumps({"context": context, "query": query, "topic_ids": topic_ids}) + "\n"
-                for chunk in response.iter_content(chunk_size=None):  # chunk_size=None for streaming
-                    chunk = chunk.decode("utf-8")
-                    response += json.loads(chunk).response
-                    yield chunk
-    
-            # SOMEHOW Put the things into the Database
-            return flask.Response(generate_response(), content_type="application/json")
-        else:
-            #cursor.execute("INSERT INTO chats (user_id, prompt, response) VALUES (%s, %s, %s) RETURNING chat_id", (user_id, query, response.json()))
-            return flask.jsonify(response.json()), 200
+        #cursor.execute("INSERT INTO chats (user_id, prompt, response) VALUES (%s, %s, %s) RETURNING chat_id", (user_id, query, response.json()))
+        return flask.jsonify({"response":response}), 200
         
         # cursor.execute("INSERT INTO chats (user_id, prompt, response) VALUES ($1, $2, $3, $4) RETURNING chat_id", )
     except Exception as e:
